3447e0666d9526a0187f2bbdf40bafea5c8e48d3
Gari Vivar <ggvivar@joy-nostalg.com>
2024-09-08 15:25:00 +0800

    added mfiles controller and config

--- config/m-files.php
+++ config/m-files.php
@@ -2,5 +2,7 @@
 
 // config for Homeful/MFiles
 return [
-
+    'Username'  => env("MFILES_ADMIN_USER","rae.admin"),
+    'Password'  => env("MFILES_ADMIN_PASS","Jng@2024"),
+    'VaultGuid' => env('MFILES_VAULTID',"DF00F177-C725-4EB7-AC98-A557D1245361") 
 ];
